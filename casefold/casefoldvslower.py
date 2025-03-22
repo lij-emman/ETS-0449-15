fccd51fe96c071cf65cae8bd068dd5cd238adfe8
ETS-0449-15 <emmanuelyonas187@gmail.com>
2025-03-23 00:33:20 +0300

    this has two other string methods related to each other they're lower and casefold

---
+++ casefold/casefoldvslower.py
@@ -0,0 +1,19 @@
+print("Example 1")
+
+text = "SoMeTIMES learning tHEories is not enough, pRacTice is required for maximum performance."
+
+
+print(" When using lower the text is as following \n ")
+print(text.lower())
+print(" \n ")
+print(" When using casefold the text is as following \n ")
+print(text.casefold())
+
+print("Example 2")
+german = "Straße und Maß"
+
+print(" When using lower the text is as following \n ")
+print(german.lower())
+print(" \n ")
+print(" When using casefold the text is as following \n ")
+print(german.casefold())
